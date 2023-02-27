038667443caf3b274944e1b5fdd2d83df6c004a6
Charles Mumo <112059620+cmumo@users.noreply.github.com>
2023-02-27 16:39:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from matplotlib  import image
+import pandas as pd
+import os
+
+st.subheader("Hello,")
+st.write("""
+Welcome to my profile! I am Charles Mumo, a final year B. Tech student majoring in Electronics and Communication Engineering. I have a keen interest in the field of data science, where I aspire to utilize my analytical skills and knowledge to develop innovative solutions to real-world problems. Through my coursework and personal projects, I have gained experience 
+in programming languages such as Python, data analysis, and machine learning techniques.
+I am excited about the endless possibilities that data science offers and look 
+forward to contributing my skills and knowledge to make a meaningful impact.
+""")
---
+++ pages/Skills.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.subheader("Skills")
+st.write('''
+
+- C/Cpp 
+- Python
+- HTML 
+- CSS 
+- Javascript
+- Excel
+- Word
+- PowerPoint
+- PowerBI
+- MySQL 
+''')
+
+st.snow()
